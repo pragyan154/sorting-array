4fffa9adca4a74198c8d6a2850b621d2e119f9fa
pragyan154 <59705399+pragyan154@users.noreply.github.com>
2020-04-10 07:49:14 -0700

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,27 @@
+#O(n^2)
+#first take the first eleemnt and swaps it with the largest element and put the largest element in the end , in  the end the smallest 
+#element arrives in the first place 
+
+
+
+def bubbleSort(arr):
+    n = len(arr)
+ 
+    # Traverse through all array elements
+    for i in range(n):
+ 
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+ 
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+ 
+bubbleSort(arr)
+ 
+print(arr)
